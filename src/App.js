069d06f326d2f11d586952f6a924e12ec9097b69
Amit Cohen <acohen@outbrain.com>
2022-07-14 12:24:32 +0300

    finish part 6

--- src/App.js
+++ src/App.js
@@ -32,11 +32,18 @@ function App() {
     setTodos(newTodos);
   };
 
+    const removeTodo = index => {
+        const newTodos = [...todos];
+
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
   return (
       <div className="app">
         <div className="todo-list">
           { todos.map((todo, index) => (
-              <Todo key={index} index={index} todo={todo} completeTodo={ completeTodo }/>
+              <Todo key={index} index={index} todo={todo} completeTodo={ completeTodo } removeTodo={ removeTodo }/>
           ))}
         <TodoForm addTodo={ addTodo } />
         </div>
